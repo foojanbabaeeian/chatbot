6fe7ec43071b9d013a3b3b0e4186f41fc5e06cea
Fozhan Babaeiyan <79134873+foojanbabaeeian@users.noreply.github.com>
2026-03-15 18:24:57 +0000

    IDk

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,55 +2,14 @@ import streamlit as st
 from openai import OpenAI
 
 # Show title and description.
-st.title("💬 Chatbot")
+st.title("Birthday Card for Elaine")
 st.write(
-    "This is a simple chatbot that uses OpenAI's GPT-3.5 model to generate responses. "
-    "To use this app, you need to provide an OpenAI API key, which you can get [here](https://platform.openai.com/account/api-keys). "
-    "You can also learn how to build this app step by step by [following our tutorial](https://docs.streamlit.io/develop/tutorials/llms/build-conversational-apps)."
-)
-
-# Ask user for their OpenAI API key via `st.text_input`.
-# Alternatively, you can store the API key in `./.streamlit/secrets.toml` and access it
-# via `st.secrets`, see https://docs.streamlit.io/develop/concepts/connections/secrets-management
-openai_api_key = st.text_input("OpenAI API Key", type="password")
-if not openai_api_key:
-    st.info("Please add your OpenAI API key to continue.", icon="🗝️")
-else:
-
-    # Create an OpenAI client.
-    client = OpenAI(api_key=openai_api_key)
-
-    # Create a session state variable to store the chat messages. This ensures that the
-    # messages persist across reruns.
-    if "messages" not in st.session_state:
-        st.session_state.messages = []
-
-    # Display the existing chat messages via `st.chat_message`.
-    for message in st.session_state.messages:
-        with st.chat_message(message["role"]):
-            st.markdown(message["content"])
-
-    # Create a chat input field to allow the user to enter a message. This will display
-    # automatically at the bottom of the page.
-    if prompt := st.chat_input("What is up?"):
-
-        # Store and display the current prompt.
-        st.session_state.messages.append({"role": "user", "content": prompt})
-        with st.chat_message("user"):
-            st.markdown(prompt)
-
-        # Generate a response using the OpenAI API.
-        stream = client.chat.completions.create(
-            model="gpt-3.5-turbo",
-            messages=[
-                {"role": m["role"], "content": m["content"]}
-                for m in st.session_state.messages
-            ],
-            stream=True,
-        )
-
-        # Stream the response to the chat using `st.write_stream`, then store it in 
-        # session state.
-        with st.chat_message("assistant"):
-            response = st.write_stream(stream)
-        st.session_state.messages.append({"role": "assistant", "content": response})
+    "This is an Alert \n \n Elaine if you are reading this you are too old now! \n \nCongradulations you are one second older than the second before and since you are a slow reader you just lost so many second you could have done so much more just reading this. \n \nThank you for wasting your prescious time by reading this on your birthday \n Happy Birthday I guess from Lily and Fooj!")
+st.balloons()
+death_note = st.button("Press for your gift from Lily")
+if death_note:
+    st.write(
+        "To eliminate your worst enemies and your closest allies \nSincerely, anonymous")
+Fooj_note = st.button("Press for your gift from Fooj")
+if Fooj_note:
+    st.write("I wish you the very best! You are so talented, kind and sweet and I am sure you will achieve great things in your life. Do not forget me when you get all old and wrinkly. ")
